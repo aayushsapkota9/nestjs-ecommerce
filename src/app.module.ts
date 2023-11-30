3f8c3135cc340074c1c9a631fac87e8777ea4f92
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-11-30 10:47:56 +0545

    added user module and bcrypt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose'; // 1.1 Import the mongoose module
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
