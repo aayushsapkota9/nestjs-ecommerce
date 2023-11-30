04ae75d11be23860fb12bb7683ee9468c4959e09
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-11-30 13:55:44 +0545

    added guards and jwt with auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose'; // 1.1 Import the mongoose module
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
